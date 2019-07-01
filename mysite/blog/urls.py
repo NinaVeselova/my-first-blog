f1fc19c75cae945fc05c21bfaf954d6229551d03
Nina <nina.veselova.job@gmail.com>
2019-07-01 21:31:19 +0300

    Changed page layout and model of the Post. Added authentication for create/edit post views and new view for non-admin users.

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('post/<int:pk>/', views.post_detail, name='post_detail'),
     path('post/new/', views.post_new, name='post_new'),
     path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
+    path('not_auth/', views.not_auth, name = 'not_auth'),
 ]
